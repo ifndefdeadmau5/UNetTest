796d680464b23e6d0238b71ff51a9f92cb1690e7
SeongMin Park <ifndefdeadmau5@gmail.com>
2016-03-06 11:35:28 +0900

    Restart 버튼 기능 변경 - NetworkManger 파괴 - 씬 재시작

---
+++ Assets/RestartApplication.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+
+public class RestartApplication : MonoBehaviour {
+
+
+    public GameObject MyNetManager;
+
+    // Use this for initialization
+    void Awake()
+    {
+        MyNetManager = GameObject.FindGameObjectWithTag("NetworkManager");
+    }
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Restart()
+    {
+        GameObject.Destroy(MyNetManager);
+        SceneManager.LoadScene("menu");
+    }
+}
